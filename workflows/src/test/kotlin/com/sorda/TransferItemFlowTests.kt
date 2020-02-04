1fec183dbc4e6ae33659baee312e2317523f25a6
Ryan Fowler <ryan.fowler@r3.com>
2020-02-04 12:36:06 +0000

    test for item transfer after expiry (without bids)

--- workflows/src/test/kotlin/com/sorda/TransferItemFlowTests.kt
+++ workflows/src/test/kotlin/com/sorda/TransferItemFlowTests.kt
@@ -1,19 +1,28 @@
 package com.sorda
 
 import com.sorda.flows.session.CreateAndListItemFlow
+import com.sorda.flows.tokens.IssueSordaTokens
 import com.sorda.states.ItemState
 import net.corda.core.concurrent.CordaFuture
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.utilities.getOrThrow
+import net.corda.node.services.Permissions
 import net.corda.testing.common.internal.testNetworkParameters
+import net.corda.testing.core.ALICE_NAME
+import net.corda.testing.core.BOB_NAME
+import net.corda.testing.driver.DriverParameters
+import net.corda.testing.driver.driver
 import net.corda.testing.node.MockNetwork
 import net.corda.testing.node.MockNetworkNotarySpec
 import net.corda.testing.node.MockNodeParameters
 import net.corda.testing.node.StartedMockNode
+import net.corda.testing.node.User
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import java.lang.Thread.sleep
 import java.time.Instant
 import kotlin.test.assertEquals
 
@@ -71,7 +80,12 @@ class TransferItemFlowTests {
         val partyB = nodeB.info.legalIdentities.single()
 
         // issue tokens
-        val item = createAndListItem("Our Item", 10.0, Instant.now()).getOrThrow()
+        val item = createAndListItem(
+            "Our Item",
+            10.0,
+            Instant.now()
+        ).getOrThrow()
+        sleep(1_000)
         val newItemState = item.coreTransaction.outputsOfType<ItemState>().single()
         assertEquals(newItemState.owner, partyA)
 
@@ -80,5 +94,30 @@ class TransferItemFlowTests {
 
     }
 
+
+    @Test
+    fun `unbid item returns to issuer after expiry`() {
+        driver(
+            DriverParameters(
+                startNodesInProcess = true,
+                extraCordappPackagesToScan = listOf("com.r3.corda.lib.tokens.contracts")
+                )
+        ) {
+            val user = User("mark", "dadada", setOf(Permissions.all()))
+            val aliceNode = startNode(providedName = ALICE_NAME, rpcUsers = listOf(user)).getOrThrow()
+            val bobNode = startNode(providedName = BOB_NAME, rpcUsers = listOf(user)).getOrThrow()
+
+            aliceNode.rpc.startFlowDynamic(IssueSordaTokens::class.java, 1000.0)
+
+            val item = aliceNode.rpc.startFlowDynamic(
+                CreateAndListItemFlow::class.java,
+                "red bike",
+                10.0,
+                Instant.now()
+            ).returnValue.getOrThrow().coreTransaction.outputsOfType(ItemState::class.java).single()
+
+            assertThat(item.owner).isEqualTo(aliceNode.nodeInfo.legalIdentities.first())
+        }
+    }
 }
 
